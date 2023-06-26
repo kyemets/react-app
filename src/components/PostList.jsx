800c54e2911bb13a1d29eae9722d23fd6b8cbef9
yemetsk <yemetskonstantin@gmail.com>
2023-06-26 10:24:16 +0200

    first commit

---
+++ src/components/PostList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PostItem from './PostItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+
+const PostList = ({posts, title, remove}) => {
+    
+    if (!posts.length) {
+        return (
+            <h1 style={{textAlign: 'center'}}>
+                List of posts is empty!
+            </h1>
+        )
+    }
+
+    return (
+        <div>
+            <h1 style={{textAlign: "center"}}>
+                {title}
+            </h1>
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                <CSSTransition
+                    key={post.id}
+                    timeout={500}
+                    classNames="post"
+                >
+                    <PostItem 
+                        number={index + 1} 
+                        post={post} 
+                        remove={remove}      
+                    />
+                </CSSTransition>
+                )}
+            </TransitionGroup>
+        </div>
+    )
+}
+
+export default PostList;
