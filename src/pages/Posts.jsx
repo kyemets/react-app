7cd93f541558351312cfdb27c55c2b2a4e35324f
yemetsk <yemetskonstantin@gmail.com>
2023-06-26 19:17:15 +0200

    added display of posts by the value

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import '../styles/App.css'
-import React, { useState, useEffect } from'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PostList from '.././components/PostList';
 import PostForm from '.././components/PostForm';
 import PostFilter from '.././components/PostFilter.jsx';
@@ -11,28 +11,34 @@ import { usePosts } from '../hooks/usePosts';
 import PostService from '../API/PostService';
 import { useFetching } from '../hooks/useFetching';
 import { getPageCount } from '../utils/pages';
+import { useObserver } from '../hooks/useObserver';
+import MySelect from '../components/UI/select/MySelect';
 
 
 function Posts () {
-  const [posts, setPosts] = useState([])
-  const [filter, setFilter] = useState({sort: '', query: ''})
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, ] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const lastElement = useRef()
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching( async(limit, page) => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    const totalCount = response.headers['x-total-count'];
-    setTotalPages(getPageCount(totalCount, limit));
-  })
+    const [fetchPosts, isPostsLoading, postError] = useFetching( async(limit, page) => {
+        const response = await PostService.getAll(limit, page);
+        setPosts([...posts, ...response.data]);
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount, limit));
+    })
 
+    useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+        setPage(page + 1)
+    });
 
-  useEffect(() => {
+    useEffect(() => {
       fetchPosts(limit, page)
-    }, [])
+    }, [page, limit])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -45,7 +51,6 @@ function Posts () {
 
   const changePage = (page) => {
     setPage(page)
-    fetchPosts(limit, page)
   }
 
   return (
@@ -67,15 +72,28 @@ function Posts () {
         filter={filter}
         setFilter={setFilter}
       />
+      <MySelect 
+        value={limit}
+        onChange={value => setLimit(value)}
+        defaultValue="Number of posts per page"
+        options={[
+            {value: 5, name: '5'},
+            {value: 10, name: '10'},
+            {value: 25, name: '25'},
+            {value: -1, name: 'all'},
+
+        ]}
+      />
       {postError &&
         <h1>Error {postError}</h1>
       }
-      {isPostsLoading 
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
-            <Loader />
-          </div>
-        : <PostList posts={sortedAndSearchedPosts} remove={removePost} title="List of JavaScript" />
+      <PostList posts={sortedAndSearchedPosts} remove={removePost} title="List of JavaScript" />
+      <div ref={lastElement} style={{height: 20, background: 'red'}} />
+
+      {isPostsLoading && 
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader /></div>
       }
+      
       <MyPagination 
         page={page}
         changePage={changePage}
