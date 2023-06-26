cc1eab7a609acbc38ccd8e5b4cce681fc7b924b3
yemetsk <yemetskonstantin@gmail.com>
2023-06-26 17:47:44 +0200

    router refactoring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './styles/App.css'
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import About from '../src/pages/About'
-import Posts from '../src/pages/Posts'
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/UI/navbar/Navbar';
+import AppRouter from './components/AppRouter';
+
 
 function App() {
 
   return (
     <BrowserRouter>
       <Navbar />
-        <Routes>
-          <Route path='/about' element={<About />} />
-          <Route path='/posts' element={<Posts />} />
-          {/* <Redirect path='/posts' element={<Posts />}/> */}
-        </Routes>
+      <AppRouter />
     </BrowserRouter>
   )
 }
---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import {routes} from "../router";
+
+const AppRouter = () => {
+
+    return (
+        <div>
+            <Routes>
+                {routes.map(route => 
+                    <Route 
+                        key={route.path} 
+                        path={route.path} 
+                        element={<route.component />}     
+                    />
+                )}
+            </Routes>
+        </div>
+    )
+}
+export default AppRouter;
